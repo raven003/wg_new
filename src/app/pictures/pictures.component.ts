ada183463bb48dd93523bd3c09d87205ebbb24a8
jan0r <julianfalk.tu@gmail.com>
2018-03-21 20:33:04 +0100

    added upload functionality

--- src/app/pictures/pictures.component.ts
+++ src/app/pictures/pictures.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PicturesComponent implements OnInit {
 
+  url: any;
   constructor() { }
 
   ngOnInit() {
   }
-
+  readUrl(event:any) {
+    if (event.target.files && event.target.files[0]) {
+      var reader = new FileReader();
+  
+      reader.onload = (event:any) => {
+        this.url = event.target.result;
+      }
+  
+      reader.readAsDataURL(event.target.files[0]);
+    }
+  }
+    
 }
